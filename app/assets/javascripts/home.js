754eabee77575e9c3585669dee5125ade397fc85
zeeshan-za-ahmad <zeeshan.za.ahmad@gmail.com>
2017-09-18 01:39:16 +0500

    reinitialized files

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,8 @@
+function showStuff(element)  {
+    var tabContents = document.getElementsByClassName('tabContent');
+    for (var i = 0; i < tabContents.length; i++) { 
+        tabContents[i].style.display = 'none';
+    }
+    var tabContentIdToShow = element.id.replace(/(\d)/g, '-$1');
+    document.getElementById(tabContentIdToShow).style.display = 'block';
+}
